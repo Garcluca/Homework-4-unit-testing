ba01c6bf721d5849f33ea3fe52d66c057ef6ab07
Lucas Garcia <32649703+Garcluca@users.noreply.github.com>
2021-02-07 23:55:23 -0800

    Create fullName_test.py

---
+++ fullName_test.py
@@ -0,0 +1,21 @@
+import unittest
+import fullName
+
+class TestAvg(unittest.TestCase):
+
+    def test_avg(self):
+        a = "Jon"
+        b = "Smith"
+
+        self.assertEqual(fullName.calculate(a,b),"Jon Smith")
+    def test_neg(self):
+        a = "Larry" 
+        b = "Page"
+        self.assertEqual(fullName.calculate(a,b),"Larry Page")
+    def test_zero(self):
+        a = "Lucas"
+        b = "Garcia"
+        self.assertEqual(fullName.calculate(a,b),"Lucas Garcia")
+
+#if __name__ = '__main__':
+unittest.main()
