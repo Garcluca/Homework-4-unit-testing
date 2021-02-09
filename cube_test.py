9ac77f5609d703e3bc526b6be59fe8c146b0d89e
Lucas Garcia <32649703+Garcluca@users.noreply.github.com>
2021-02-08 22:10:10 -0800

    Update cube_test.py

--- cube_test.py
+++ cube_test.py
@@ -4,11 +4,11 @@ import cube
 class TestCube(unittest.TestCase):
 
     def test_vol(self):
-        self.assertEqual(cube.calculate(1,2,3),(1*2*3))
+        self.assertEqual(cube.calculate(1,2,3),6)
     def test_neg(self):
-        self.assertEqual(cube.calculate(-1,-2,-3),(1*2*3))
+        self.assertEqual(cube.calculate(-1,-2,-3),6)
     def test_zero(self):
-        self.assertEqual(cube.calculate(0,0,0),(0))
+        self.assertEqual(cube.calculate(0,2,3),(0))
 
 
 unittest.main()
